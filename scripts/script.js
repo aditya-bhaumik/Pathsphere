3c9bbef78b4a9f63968794901300fd6da61f27ba
jaison-jai-john <jaisonjaison2004@gmail.com>
2024-10-06 08:52:35 +0530

    FIX: dark mode toggle got broken on last commit. fixed it

--- scripts/script.js
+++ scripts/script.js
@@ -27,29 +27,6 @@ gsap.from('.btn', {
   delay: 1.5,
 });
 
-let icon = document.getElementById('icon');
-window.onload = function () {
-  let theme = localStorage.getItem('theme');
-  if (theme === 'dark') {
-    document.body.classList.add('darkmode');
-    icon.src = '/images/light.png';
-  } else {
-    document.body.classList.remove('darkmode');
-    icon.src = '/images/dark.png';
-  }
-};
-
-icon.onclick = function () {
-  document.body.classList.toggle('darkmode');
-  if (document.body.classList.contains('darkmode')) {
-    icon.src = '/images/light.png';
-    localStorage.setItem('theme', 'dark');
-  } else {
-    icon.src = '/images/dark.png';
-    localStorage.setItem('theme', 'light');
-  }
-};
-
 document.querySelectorAll('.faq-question').forEach((question) => {
   question.addEventListener('click', () => {
     const answer = question.nextElementSibling;
