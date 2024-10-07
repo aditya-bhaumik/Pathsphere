408a6dd5625750b5f52fef5ee34ec0fd79552228
jaison-jai-john <jaisonjaison2004@gmail.com>
2024-10-07 09:18:25 +0530

    testing loadingscreen

--- scripts/componentLoader.js
+++ scripts/componentLoader.js
@@ -1,3 +1,4 @@
+const componentsToBeLoaded = [];
 const fetchComponent = async (componentName) => {
   try {
     const res = await fetch(
@@ -28,27 +29,40 @@ const prependComponent = async (
   componentName,
   targetElement = document.body
 ) => {
-  const [component, styles, scripts] = await fetchComponent(componentName);
+  componentsToBeLoaded.push(componentName);
+  try {
+    const [component, styles, scripts] = await fetchComponent(componentName);
+    styles.forEach((style) => {
+      document.head.appendChild(style);
+    });
 
-  styles.forEach((style) => {
-    document.head.appendChild(style);
-  });
+    component.forEach((element) => {
+      while (!targetElement) {
+        targetElement = document.body;
+      }
+      targetElement.prepend(element);
+    });
 
-  component.forEach((element) => {
-    targetElement.prepend(element);
-  });
-
-  scripts.forEach((script) => {
-    const scriptTag = document.createElement('script');
-    scriptTag.src = script.src;
-    document.body.appendChild(scriptTag);
-  });
+    scripts.forEach((script) => {
+      const scriptTag = document.createElement('script');
+      scriptTag.src = script.src;
+      document.body.appendChild(scriptTag);
+    });
+    componentsToBeLoaded.pop();
+    console.log('Component loaded:', componentName);
+  } catch (e) {
+    console.error(
+      'error while fetching component, please check if the given name is correct',
+      e
+    );
+  }
 };
 
 const appendComponent = async (
   componentName,
   targetElement = document.body
 ) => {
+  componentsToBeLoaded.push(componentName);
   const [component, styles, scripts] = await fetchComponent(componentName);
 
   styles.forEach((style) => {
@@ -64,12 +78,14 @@ const appendComponent = async (
     scriptTag.src = script.src;
     document.body.appendChild(scriptTag);
   });
+  componentsToBeLoaded.pop();
 };
 
 const insertComponentBefore = async (
   componentName,
   BeforeElement = document.body
 ) => {
+  componentsToBeLoaded.push(componentName);
   const [component, styles, scripts] = await fetchComponent(componentName);
 
   styles.forEach((style) => {
@@ -85,12 +101,14 @@ const insertComponentBefore = async (
     scriptTag.src = script.src;
     document.body.appendChild(scriptTag);
   });
+  componentsToBeLoaded.pop();
 };
 
 const insertComponentAfter = async (
   componentName,
   AfterElement = document.body
 ) => {
+  componentsToBeLoaded.push(componentName);
   const [component, styles, scripts] = await fetchComponent(componentName);
 
   styles.forEach((style) => {
@@ -106,4 +124,37 @@ const insertComponentAfter = async (
     scriptTag.src = script.src;
     document.body.appendChild(scriptTag);
   });
+  componentsToBeLoaded.pop();
 };
+
+const hideContent = () => {
+  const bodyChildren = document.body.children;
+  Array.from(bodyChildren).forEach((child) => {
+    child.style.visibility = 'hidden'; // You can also use 'display: none' if needed
+  });
+};
+
+const showContent = () => {
+  const bodyChildren = document.body.children;
+  Array.from(bodyChildren).forEach((child) => {
+    child.style.visibility = 'visible'; // You can also use 'display: block' if needed
+  });
+};
+
+const waitTillComponentsLoaded = async () => {
+  while (componentsToBeLoaded.length > 0) {
+    await new Promise((resolve) => setTimeout(resolve, 100));
+  }
+  const loadingScreen = document.getElementById('LoadingScreen');
+  loadingScreen.classList.remove('loading');
+  loadingScreen.classList.add('loaded');
+  showContent();
+};
+
+document.addEventListener('DOMContentLoaded', hideContent);
+
+console.log('Loading components...');
+
+prependComponent('LoadingScreen');
+prependComponent('GlobalStyles');
+prependComponent('GlobalScripts');
